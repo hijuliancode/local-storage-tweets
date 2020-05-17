380ea3ba18fcbeea73c13bc7c989fb56251ef3c6
JulianCode <hijuliansosa@protonmail.com>
2020-05-16 23:36:23 -0500

    Starting the Project

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+// vars
+const form = document.getElementById('formulario')
+const tweetsList = document.getElementById('lista-tweets')
+
+// event listeners
+eventListeners()
+function eventListeners() {
+  // form submit
+  form.addEventListener('submit', addTweet)
+}
+
+// functions
+function addTweet(e) {
+  e.preventDefault()
+  // read textarea value
+  const tweet = document.getElementById('tweet').value;
+  // create delete button
+  const deleteBtn = document.createElement('a')
+  deleteBtn.classList = 'borrar-tweet'
+  deleteBtn.textContent = 'X'
+
+
+  // create element and add to list
+  const li = document.createElement('li')
+  li.textContent = tweet
+  li.appendChild(deleteBtn)
+  tweetsList.appendChild(li)
+
+}
